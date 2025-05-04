c5a29b915b63960c016a6c147af8a92ee87f4080
Vedardha Gudapati <55837018+VedaBoy@users.noreply.github.com>
2025-05-04 21:03:23 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/RUTHS/', // 👈 important for GitHub Pages
+  base: '/', // 👈 important for GitHub Pages
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
